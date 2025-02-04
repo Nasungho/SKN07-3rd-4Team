caa8106ee1d8cf31992da480e0a09389121f28aa
dongik <sdimivy014@gmail.com>
2025-02-04 14:56:31 +0900

    update app.py add csv load feature

--- chainlit/app.py
+++ chainlit/app.py
@@ -1,85 +1,172 @@
 from api import api_keys
-from utils.client import embed_and_store_message, search_similar_messages
 
-from operator import itemgetter
+from typing import List
+from pathlib import Path
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain_community.document_loaders import (
+    PyMuPDFLoader,
+    CSVLoader
+)
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain.vectorstores.chroma import Chroma
+from langchain_community.vectorstores import Chroma
+from langchain.indexes import SQLRecordManager, index
+from langchain.schema import Document
+from langchain.schema.runnable import Runnable, RunnablePassthrough, RunnableConfig
+from langchain.callbacks.base import BaseCallbackHandler
 
-# from langchain_community.chat_models import ChatOpenAI
-from langchain_openai import ChatOpenAI
-from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.schema.output_parser import StrOutputParser
-from langchain.schema.runnable import Runnable, RunnablePassthrough, RunnableLambda
-from langchain.schema.runnable.config import RunnableConfig
-from langchain.memory import ConversationBufferMemory
-
-from chainlit.types import ThreadDict
 import chainlit as cl
 
-def setup_runnable():
-    memory = cl.user_session.get("memory")  # type: ConversationBufferMemory
-    model = ChatOpenAI(streaming=True)
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", "You are a helpful chatbot"),
-            MessagesPlaceholder(variable_name="history"),
-            ("human", "{question}"),
-        ]
+
+chunk_size = 1024
+chunk_overlap = 50
+
+embeddings_model = OpenAIEmbeddings()
+
+PDF_STORAGE_PATH = "data/pdfs"
+CSV_STORAGE_PATH = "data/csv"
+
+def process_pdfs(pdf_storage_path: str):
+    pdf_directory = Path(pdf_storage_path)
+    docs = []  # type: List[Document]
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+
+    for pdf_path in pdf_directory.glob("*.pdf"):
+        loader = PyMuPDFLoader(str(pdf_path))
+        documents = loader.load()
+        docs += text_splitter.split_documents(documents)
+
+    doc_search = Chroma.from_documents(docs, embeddings_model)
+
+    namespace = "chromadb/my_documents"
+    record_manager = SQLRecordManager(
+        namespace, db_url="sqlite:///record_manager_cache.sql"
+    )
+    record_manager.create_schema()
+
+    index_result = index(
+        docs,
+        record_manager,
+        doc_search,
+        cleanup="incremental",
+        source_id_key="source",
     )
 
+    print(f"Indexing stats: {index_result}")
+
+    return doc_search
+
+def process_csv(csv_storage_path: str):
+    csv_directory = Path(csv_storage_path)
+    docs = []
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    
+    for csv_path in csv_directory.glob("*.csv"):
+        loader = CSVLoader(str(csv_path))
+        documents = loader.load()
+        docs += text_splitter.split_documents(documents)
+    
+    doc_search = Chroma.from_documents(docs, embeddings_model)
+    
+    namespace = "chromadb/my_documents"
+    record_manager = SQLRecordManager(
+        namespace, db_url="sqlite:///record_manager_cache.sql"
+    )
+    record_manager.create_schema()
+    
+    index_result = index(
+        docs,
+        record_manager,
+        doc_search,
+        cleanup="incremental",
+        source_id_key="source",
+    )
+    
+    print(f"Indexing stats: {index_result}")
+    
+    return doc_search
+
+def load_collection(collection_name: str):
+
+    doc_search = Chroma(
+        persist_directory="./db",
+        embedding_function=OpenAIEmbeddings(),
+        collection_name=collection_name
+    )
+    return doc_search
+
+# doc_search = process_pdfs(PDF_STORAGE_PATH)
+# doc_search = load_pdfs(PDF_STORAGE_PATH)
+
+doc_search = load_collection('csv')
+model = ChatOpenAI(model_name="gpt-4", streaming=True)
+
+
+@cl.on_chat_start
+async def on_chat_start():
+    template = """아래 주어진 context를 통해 Question에 대답해:
+
+    {context}
+
+    Question: {question}
+    """
+    prompt = ChatPromptTemplate.from_template(template)
+
+    def format_docs(docs):
+        return "\n\n".join([d.page_content for d in docs])
+
+    retriever = doc_search.as_retriever()
+
     runnable = (
-        RunnablePassthrough.assign(
-            history=RunnableLambda(memory.load_memory_variables) | itemgetter("history")
-        )
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
         | prompt
         | model
         | StrOutputParser()
     )
+
     cl.user_session.set("runnable", runnable)
 
-@cl.on_chat_start
-async def on_chat_start():
-    cl.user_session.set("memory", ConversationBufferMemory(return_messages=True))
-    setup_runnable()
-
-@cl.on_chat_resume
-async def on_chat_resume(thread: ThreadDict):
-    memory = ConversationBufferMemory(return_messages=True)
-    root_messages = [m for m in thread["steps"] if m["parentId"] == None]
-
-    for message in root_messages:
-        if message["type"] == "user_message":
-            memory.chat_memory.add_user_message(message["output"])
-        else:
-            memory.chat_memory.add_ai_message(message["output"])
-        
-        # Milvus에 메시지 벡터화하여 저장
-        embed_and_store_message(message["output"])
-
-    cl.user_session.set("memory", memory)
-    setup_runnable()
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    memory = cl.user_session.get("memory")  # type: ConversationBufferMemory
     runnable = cl.user_session.get("runnable")  # type: Runnable
-    res = cl.Message(content="")
+    msg = cl.Message(content="")
 
-    # 유사한 메시지 검색 (Milvus 사용)
-    similar_messages = search_similar_messages(message.content)
-    
-    # 유사한 메시지를 history에 추가하여 모델에 제공
-    memory.chat_memory.add_user_message(message.content)
-    memory.chat_memory.add_ai_message(res.content)
+    class PostMessageHandler(BaseCallbackHandler):
+        """
+        Callback handler for handling the retriever and LLM processes.
+        Used to post the sources of the retrieved documents as a Chainlit element.
+        """
+
+        def __init__(self, msg: cl.Message):
+            BaseCallbackHandler.__init__(self)
+            self.msg = msg
+            self.sources = set()  # To store unique pairs
+
+        def on_retriever_end(self, documents, *, run_id, parent_run_id, **kwargs):
+            for d in documents:
+                source_page_pair = (d.metadata['source'], d.metadata['page'])
+                self.sources.add(source_page_pair)  # Add unique pairs to the set
+
+        def on_llm_end(self, response, *, run_id, parent_run_id, **kwargs):
+            if len(self.sources):
+                sources_text = "\n".join([f"{source}#page={page}" for source, page in self.sources])
+                self.msg.elements.append(
+                    cl.Text(name="Sources", content=sources_text, display="inline")
+                )
 
     async for chunk in runnable.astream(
-        {"question": message.content},
-        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+        message.content,
+        config=RunnableConfig(callbacks=[
+            cl.LangchainCallbackHandler(),
+            PostMessageHandler(msg)
+        ]),
     ):
-        await res.stream_token(chunk)
+        await msg.stream_token(chunk)
 
-    await res.send()
-
-    # Milvus에 벡터화된 메시지 저장
-    embed_and_store_message(res.content)
+    await msg.send()
 
 
 
